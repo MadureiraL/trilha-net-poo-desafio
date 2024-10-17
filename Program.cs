2f3dbfb7adcf81f7b94dcaf306cefcb62b3f8f30
Levi Madureira <abreumadureira@gmail.com>
2024-10-17 16:31:03 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "1", imei:"1212121212", memoria: 124  );
+iphone.Ligar();
+iphone.InstalarAplicativo("Tinder");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "7891011", modelo: "2", imei: "3434343434", memoria: 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
